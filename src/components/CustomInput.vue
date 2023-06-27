f087789c6c13b077425aad04811328e9a6ddb9ed
Nikita Avdeev <nikita.avdeev@xpage.ru>
2023-06-27 15:24:28 +0500

    feat(CustomInput)

---
+++ src/components/CustomInput.vue
@@ -0,0 +1,32 @@
+<script>
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+	name: "CustomInput",
+	props: ['modelValue', "placeholderValue", "typeValue"],
+	emits: ['update:modelValue'],
+})
+</script>
+
+<template>
+	<label class="input">
+		<input :type="typeValue" :placeholder="placeholderValue" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" class="input__input">
+	</label>
+</template>
+
+<style scoped lang="sass">
+.input
+	width: 100%
+	border-bottom: 2px solid #6F91B4
+	padding: 8px 0
+	&:not(:last-child)
+	&__input
+		width: 100%
+		border: unset
+		font-size: 16px
+		font-weight: 500
+		line-height: 24px
+		color: #6F91B4
+		margin: 8px 0
+		padding: 0
+</style>
